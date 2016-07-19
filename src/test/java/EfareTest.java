90ae40add1c5b55164ea022841272bb3379137b7
pawanlowanshi <pawan92lowanshi@gmail.com>
2016-07-19 15:16:08 +0530

    Update EfareTest.java

--- src/test/java/EfareTest.java
+++ src/test/java/EfareTest.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Properties;
 
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 import org.testng.xml.XmlTest;
 
@@ -12,19 +11,16 @@ import com.omniwyse.selenium.data.DataManager;
 
 public class EfareTest {
 
-	RemoteWebDriver driver;
-
 	@Test
-	public void f(XmlTest config) throws Exception {
+	public void efareTest(XmlTest config) throws Exception {
 		DataManager.config = config;
 		Properties properties = new Properties();
 		properties.load(new FileInputStream(new File("config.properties")));
-		String repPath=properties.getProperty("rootPath");
+		String repPath = properties.getProperty("rootPath");
 		DataManager.currentDate = new SimpleDateFormat("dd-MM-yyyy").format(Calendar.getInstance().getTime());
 		DataManager.timestamp = new SimpleDateFormat("HH_mm_ss").format(Calendar.getInstance().getTime());
 		System.out.println(DataManager.currentDate);
-		//System.out.println("this is our repository path " +  properties.getProperty("rootPath")+"  "+repPath);
-		new DataManager().executeTestPlan( repPath.replace('\\', '/'), DataManager.currentDate);
+		new DataManager().executeTestPlan(repPath.replace('\\', '/'), DataManager.currentDate);
 		System.out.println("Input(TestPlan) file reading is done..");
 	}
 }
